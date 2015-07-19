45f511dae2fb114fcdc74b474c67aff799aff2a5
karanaditya993 <karanaditya993@gmail.com>
2015-07-19 14:36:45 -0700

    Materialize demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
       redirect_to new_user_session_path
     else
       @user = User.find(current_user.id)
+      render partial: 'greeting', locals: {user: @user}, :layout => 'layouts/application'
     end
   end
 
