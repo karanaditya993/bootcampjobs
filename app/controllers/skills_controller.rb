fb4a9d2357333db87da6abb2469ddddff10ccede
karanaditya993 <karanaditya993@gmail.com>
2015-07-19 18:11:06 -0700

    User many to many skills working

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,6 @@
 class SkillsController < ApplicationController
   def index
-    @skills = Skill.all
+    @skills = current_user.skills
   end
 
   def new
@@ -10,9 +10,9 @@ class SkillsController < ApplicationController
   end
 
   def create
-    @skill = Skill.create(skill_params)
+    @skill = Skill.where(skills: skill_params).first_or_create
     @user_skill = UserSkill.new(user_id: current_user.id, skill_id: @skill.id)
-    if @user_skill.save
+    if @skill.save && @user_skill.save
       redirect_to user_skill_path(@skill, @user_skill)
     else
       render 'new'
@@ -23,18 +23,15 @@ class SkillsController < ApplicationController
     @skills = current_user.skills
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   def destroy
   end
 
   private
   def skill_params
-    params.require(:skill).permit(:id, :title)
+    params.require(:skill).permit(:title)
   end
 
 end
+
+@skills = Skill.all
+#is an array of skills
