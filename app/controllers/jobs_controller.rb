41340dae4efaa9e9e1e570170fb570cd6fa518ae
karanaditya993 <karanaditya993@gmail.com>
2015-07-19 19:43:05 -0700

    search for jobs working

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,8 @@
 class JobsController < ApplicationController
   def index
-    @jobs = Job.all
+    if params[:search]
+      @jobs = Job.search(params[:search]).order('created_at DESC')
+    end
   end
 
   def show
