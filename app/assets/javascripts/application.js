9ce924fa6b5e9942465b6a88bbf9d477f2510230
Mike Farr <mikefarr@mac.com>
2015-07-22 13:38:26 -0700

    added iframe into bootcamp jobs, changed application.html and jobs/index.html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(document).on("page:change", function() {
-  githubFadeOut();
-});
+//$(document).on("page:change", function() {
+//  githubFadeOut();
+//});
+//
+//var githubFadeOut = function(){
+//  $('#github-signin-button').on("click", function(event){
+//    setTimeout(function(){ event }, 1000)
+//    $('#github-signin-button').fadeOut();
+//  });
+//}
 
-var githubFadeOut = function(){
-  $('#github-signin-button').on("click", function(event){
-    setTimeout(function(){ event }, 1000)
-    $('#github-signin-button').fadeOut();
-  });
+
+function reload_iframe(loc) {
+    document.getElementById('embedded_page').src = loc;
+//            document.getElementById('iframe_handle').style.top = pos;
+//            document.getElementById('iframe_handle').style.backgroundColor = frameHandleColor;
+}
+function new_iframe(loc) {
+    document.getElementById('embedded_page').src = loc;
 }
